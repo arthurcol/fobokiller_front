abe6cd1dd78c33147b02f68151e9ea55e14c69b3
hugochanteloup <hugo.c@club-internet.fr>
2021-12-02 17:19:26 +0100

    presque fini

--- fobokiller_front/app.py
+++ fobokiller_front/app.py
@@ -19,29 +19,38 @@ df = pd.read_csv(path, index_col=0)
 
 
 #URL API
-url_details_base = 'https://api8-2rnijzpfva-ew.a.run.app/details?alias='
-url_api_rate = 'https://api8-2rnijzpfva-ew.a.run.app/summary_reviews?'
-url_api = 'https://api8-2rnijzpfva-ew.a.run.app/summary_reviews2?'
+url_details_base = 'https://api9-2rnijzpfva-ew.a.run.app/details?alias='
+url_api_rate = 'https://api9-2rnijzpfva-ew.a.run.app/summary_reviews?'
+url_api = 'https://api9-2rnijzpfva-ew.a.run.app/summary_reviews2?'
+
 
 st.set_page_config(
-        page_title="FOBO Kiler", # => Quick reference - Streamlit
-        page_icon="🍖",
-        layout="centered", # wide
-        initial_sidebar_state="auto") # collapsed
+    page_title="FOBO Kiler",  # => Quick reference - Streamlit
+    page_icon="🍖",
+    layout="wide",  # wide
+    initial_sidebar_state="auto")  # collapsed
+
 
 st.markdown(
-    '<h1 style="text-align:center; font-size:100px;font-family:Gill Sans, sans-serif;">🍗 FOBO Killer ! 🍗</h1>',
+    '''<h1 style="text-align:center;
+    font-size:100px;font-family:Gill Sans, sans-serif;">
+    🍗 FOBO Killer ! 🍗</h1>''',
     unsafe_allow_html=True)
-
+m = folium.Map(location=[48.85584630805084, 2.3452032500919433],
+               zoom_start=12,
+               position='')
 
 st.markdown("""<br/><br/><br/>""", unsafe_allow_html=True)
+
+
 st.markdown(
-    """<p style="font-size:23px;margin-bottom:-100px">What do you want to eat today ? 😋</p>""",
+    """<p style="font-size:23px;margin-bottom:-100px">
+    What do you want to eat today ? 😋</p>""",
     unsafe_allow_html=True)
 query_food = st.text_input(placeholder="I would like to eat a vegie burger in a cosy place",
                            label='')
 st.markdown("""<br/><br/>""", unsafe_allow_html=True)
-st.markdown('<p style="font-size:23px;">How FOBOic are you ?</p>',
+st.markdown('<p style="font-size:23px;">How <big>FOBO</big>ic are you ?</p>',
             unsafe_allow_html=True)
 nb = st.slider('', 2, 10)
 st.markdown("""<body>
@@ -61,7 +70,7 @@ if st.button('Surprise me!'):
 
         #init map
         m = folium.Map(location=[48.85584630805084, 2.3452032500919433],
-                       zoom_start=12)
+                       zoom_start=12, position='')
 
         #get lat and long of restaurants
         #request api
@@ -86,8 +95,7 @@ if st.button('Surprise me!'):
 
         #create big df
         result_df = details_df.join(result_reviews_df).join(
-            df_rate['rate_filtered'])
-        st.write(result_df)#t drop
+            df_rate['rate_filtered']).sort_values('metric sim_ratio', ascending=False)
 
 
 
@@ -133,48 +141,34 @@ if st.button('Surprise me!'):
         folium.Marker(location=[48.86489231778049, 2.3799136342856975],
                     icon=folium.Icon(color="red", icon='user')).add_to(m)
 
-        #display map
-        st.markdown("""<h3 style="text-align:center;
-                    font-family:Baskerville, Baskerville Old Face, Garamond, Times New Roman, sans-serif">Where you can enjoy your meal ! 🤤 </h3> """,
-                    unsafe_allow_html=True)
-        st.markdown("<br/>", unsafe_allow_html=True)
-        folium_static(m, width=1440, height=400)
-
-        #st.table(result_df[['nb_sentences', 'nb_review','metric sim_ratio','sentences_pond','metric_pond']])
-        metrics_df = result_df.sort_values('metric sim_ratio', ascending=False)
-
-        df_test = pd.DataFrame(
-            [['Pizza-Vesuvio', 4.2, '1 rue Gozlin ', .32],
-             ['Pizza Julia', 4, '43 rue de Charenton ', 0.2665],
-             ['Il Brigante', 4.9, '14 rue Ruisseau ', 0.2809],
-             ['Pizza Di Loretta', 4.3, '62 rue Rodier ', .35],
-             ['court', 4.6, '14 rue Normandie. ', 0.310],
-             ['nom super long pour faire chier', 4.5, '42 rue Tiquetonne.',.17],
-             ['O\'Scia', 4.9, '8 rue des Gravilliers.', 0.3159]],
-            columns=['name',
-                     'rate',
-                     'adresse',
-                     'metric']).sort_values('metric', ascending=False)
-
-        df_test['metric_adjust'] = df_test['metric']*2.5
 
         st.markdown("""<h3 style="text-align:center;
-                    font-family:Baskerville, Baskerville Old Face, Garamond, Times New Roman, sans-serif">Why you have to go </h3> """,
+                    font-family:Baskerville, Baskerville Old Face, Garamond, Times New Roman, sans-serif">
+                    Where you can enjoy your meal ! 🤤 </h3> """,
                     unsafe_allow_html=True)
         st.markdown("<br/>", unsafe_allow_html=True)
 
-        col1, col2 = st.columns(2)
+        #display maps and res
+
+        col= st.columns((2,1,2))
         for i in range(nb):
-            col1.markdown(
-                f"""<span style='font-size:30px;'> {result_df['name'][i]} </span>
-                <progress id="file" max="0.5" value="{result_df['metric sim_ratio'][i]}" style="margin-right:0px"></progress>
-                """,
+            col[1].markdown(
+                f"""<progress id="file" max="0.4" value="{result_df['metric sim_ratio'][i]}" style="margin-right:0px"></progress><br/><br/><br/>""",
                 unsafe_allow_html=True)
+            col[0].markdown(
+                f"""<h3>{result_df['name'][i]}</h3>
+                """ + """<style>
+                .button {
+                    font-size: 30px;
+                    background-color: #fafafa;
+                    border: none;
+                    }
+            """,
+                unsafe_allow_html=True)
+        with col[2]:
+            folium_static(m, width=700, height=400)
 
-        for alias in aliases:
-            for i in range(10):
-                if len(result_df["reviews_heatmaps_html"][alias][i]) < 600:
-                    col2.markdown(f'{result_df["reviews_heatmaps_html"][alias][i]} <br/>',
-                                unsafe_allow_html=True)
-
-                    break
+        st.markdown(f"""<span id='span_txt' style='display:none;'><h3 style="text-align:center;
+                        font-family:Baskerville, Baskerville Old Face, Garamond, Times New Roman, sans-serif">
+                        Why you have to go </h3><br/>
+                        </span> """, unsafe_allow_html=True)
