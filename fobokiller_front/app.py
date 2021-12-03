2fcc19d9944e88e52ff2e5b28689a49316d5208d
hugochanteloup <hugo.c@club-internet.fr>
2021-12-03 11:03:54 +0100

    Version final

--- fobokiller_front/app.py
+++ fobokiller_front/app.py
@@ -33,7 +33,7 @@ st.set_page_config(
 st.markdown(
     '''<h1 style="text-align:center;
     font-size:100px;font-family:Gill Sans, sans-serif;">
-    🍗 FOBO Killer ! 🍗</h1>''',
+    🍗 FOBO Killer ! 🍗</h1><br/><br/>''',
     unsafe_allow_html=True)
 m = folium.Map(location=[48.85584630805084, 2.3452032500919433],
                zoom_start=12,
@@ -42,14 +42,15 @@ m = folium.Map(location=[48.85584630805084, 2.3452032500919433],
 
 
 st.markdown(
-    """<p style="font-size:23px;margin-bottom:-100px">
+    """<p style="font-size:23px;margin-bottom:-100px;">
     What do you want to eat today ? 😋</p>""",
     unsafe_allow_html=True)
-query_food = st.text_input(placeholder="I would like to eat a vegie burger in a cosy place",
+query_food = st.text_input(placeholder="I would like to eat burger ...",
                            label='')
 st.markdown("""<br/><br/>""", unsafe_allow_html=True)
-st.markdown('<p style="font-size:23px;">How <big>FOBO</big>ic are you ?</p>',
-            unsafe_allow_html=True)
+st.markdown(
+    '<p style="font-size:23px;margin-bottom:-10px;">How <big>FOBO</big>ic are you ?</p>',
+    unsafe_allow_html=True)
 nb = st.slider('', 2, 10)
 st.markdown("""<body>
             <div id="col">
@@ -64,99 +65,105 @@ st.markdown("""<body>
             unsafe_allow_html=True)
 
 if st.button('Surprise me!'):
-    #with st.spinner(text='Looking for the best restaurant for you...'):
+    with st.spinner(text='Looking for the best restaurant for you...'):
 
-    #init map
-    m = folium.Map(location=[48.85584630805084, 2.3452032500919433],
-                   zoom_start=12, position='')
+        #init map
+        m = folium.Map(location=[48.85584630805084, 2.3452032500919433],
+                    zoom_start=12, position='')
 
-    #get lat and long of restaurants
-    #request api
-    params={'text':query_food,
-            'n_best':nb,
-            'min_review':10}
+        #get lat and long of restaurants
+        #request api
+        params={'text':query_food,
+                'n_best':nb,
+                'min_review':10}
 
-    #df de base + get aliases for second request
-    result_reviews = requests.get(url_api, params=params).json()
-    result_reviews_df = pd.DataFrame(result_reviews)
-    aliases = list(result_reviews_df['review_clean'].keys())
+        #df de base + get aliases for second request
+        result_reviews = requests.get(url_api, params=params).json()
+        result_reviews_df = pd.DataFrame(result_reviews)
+        aliases = list(result_reviews_df['review_clean'].keys())
 
 
-    #request second api
-    url_details=url_details_base+'&alias='.join(aliases)
+        #request second api
+        url_details=url_details_base+'&alias='.join(aliases)
 
-    details = requests.get(url_details + '&alias='.join(aliases)).json()
-    details_df = pd.DataFrame(details)
-    details_df.set_index('alias', inplace=True)
+        details = requests.get(url_details + '&alias='.join(aliases)).json()
+        details_df = pd.DataFrame(details)
+        details_df.set_index('alias', inplace=True)
 
-    #create big df
-    result_df = details_df.join(result_reviews_df).sort_values(
-        'metric sim_ratio', ascending=False)
+        #create big df
+        result_df = details_df.join(result_reviews_df).sort_values(
+            'metric sim_ratio', ascending=False)
 
-    for alias in result_df.index:
-        folium.Marker(location=[
-            result_df['latitude'][alias], result_df['longitude'][alias]
-        ],
-                      icon=folium.Icon(color="blue",
-                                       icon='mapmarker',
-                                       angle=30),
-                      popup=folium.Popup(html=f"""
-                                    <body>
-                                    <div id="title">
-                                        <h3>{result_df['name'][alias]}</h3>
-                                    </div>
+        for alias in result_df.index:
+            folium.Marker(location=[
+                result_df['latitude'][alias], result_df['longitude'][alias]
+            ],
+                        icon=folium.Icon(color="blue",
+                                        icon='mapmarker',
+                                        angle=30),
+                        popup=folium.Popup(html=f"""
+                                        <body>
+                                        <div id="title">
+                                            <h3>{result_df['name'][alias]}</h3>
+                                        </div>
 
-                                    <div id="col">
-                                        <p style=
-                                        "color:#191970;
-                                        font-size: 20px;
-                                        border-right:0px;
-                                        text-align:left;
-                                        ">{round(result_df['rate_filtered_y'][alias],1)}/5</p>
-                                        <p
-                                        style="
-                                        font-size:15px;
-                                        border-left:0px;
+                                        <div id="col">
+                                            <p style=
+                                            "color:#191970;
+                                            font-size: 20px;
+                                            border-right:0px;
+                                            text-align:left;
+                                            ">{round(result_df['rate_filtered_y'][alias],1)}/5</p>
+                                            <p
+                                            style="
+                                            font-size:15px;
+                                            border-left:0px;
 
-                                        "> {result_df['address'][alias]}.</p>
-                                    </div>
-                                    </body>
-                                    """ + """
-                                    <style>
-                                    #title {
-                                        width=400px;
+                                            "> {result_df['address'][alias]}.</p>
+                                        </div>
+                                        </body>
+                                        """ + """
+                                        <style>
+                                        #title {
+                                            width=400px;
+                                            }
+                                        #col {
+                                        column-count: 2;
                                         }
-                                    #col {
-                                    column-count: 2;
-                                    }
-                                    </style>""")).add_to(m)
+                                        </style>""")).add_to(m)
 
-    #localisation où suis-je ?
-    folium.Marker(location=[48.86489231778049, 2.3799136342856975],
-                icon=folium.Icon(color="red", icon='user')).add_to(m)
+        #localisation où suis-je ?
+        folium.Marker(location=[48.86489231778049, 2.3799136342856975],
+                    icon=folium.Icon(color="red", icon='user')).add_to(m)
 
 
-    st.markdown("""<h3 style="text-align:center;
-                    font-family:Baskerville, Baskerville Old Face, Garamond, Times New Roman, sans-serif">
-                    Where you can enjoy your meal ! 🤤 </h3> """,
+        st.markdown("""<h3 style="text-align:center;
+                        font-family:Baskerville, Baskerville Old Face, Garamond, Times New Roman, sans-serif">
+                        Where you can enjoy your meal ! 🤤 </h3> """,
+                    unsafe_allow_html=True)
+        st.markdown("<br/>", unsafe_allow_html=True)
+
+        #display maps and res
+        #list_restau = result_df.index
+        #direction = st.radio('Restaurant', (result_df.index))
+        col= st.columns((1,1,2))
+        for i in range(nb):
+            col[1].markdown(
+                f"""<progress id="file" max="0.5" value="{result_df['metric sim_ratio'][i]}" style="margin-right:0px"></progress><br/><br/><br/>""",
                 unsafe_allow_html=True)
-    st.markdown("<br/>", unsafe_allow_html=True)
+            with col[0]:
+                st.markdown(f"<h3>{result_df['name'][i]}</h3><br/>", unsafe_allow_html=True)
+            #if direction == result_df.index[i]:
 
-    #display maps and res
-    list_restau = result_df.index
-    direction = st.radio('Restaurant', (result_df.index))
-    col= st.columns((1,1,2))
-    for i in range(nb):
-        col[1].markdown(
-            f"""<progress id="file" max="0.5" value="{result_df['metric sim_ratio'][i]}" style="margin-right:0px"></progress><br/><br/><br/>""",
-            unsafe_allow_html=True)
-        with col[0]:
-            st.markdown(f"<h3>{list_restau[i]}</h3>", unsafe_allow_html=True)
-        #if direction == result_df.index[i]:
-        sentence_heat = '\n'.join(result_df["reviews_heatmaps_html"][i])
-        st.markdown(f"""{sentence_heat}""", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align:center;'> Why you have to go!!!<h2/>", unsafe_allow_html=True)
+        for i in range(nb):
+            with st.expander(result_df['name'][i]):
+                review_col = list(set(result_df["reviews_heatmaps_html"][i]))
+
+                sentence_heat ='\n - ' + '\n \n - '.join(review_col[:5])
+                st.markdown(f"""<br/><br/>{sentence_heat}""", unsafe_allow_html=True)
 
 
 
-    with col[2]:
-        folium_static(m, width=600, height=400)
+        with col[2]:
+            folium_static(m, width=600, height=400)
